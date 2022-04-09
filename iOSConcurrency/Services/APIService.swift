d20359f0377feec9722bf12f29ff2a2c5625254f
davidakoontz <david@koontz.name>
2022-04-09 14:50:06 -0500

    A  barely  functioning  first pass at APIService  - only works with User[]  -  no real error handling

--- iOSConcurrency/Services/APIService.swift
+++ iOSConcurrency/Services/APIService.swift
@@ -10,12 +10,13 @@ import Foundation
 struct APIService {
     let urlString: String
     
-    func getUsers(completion: @escaping ([User]) -> Void) {
+    func getUsers(completion: @escaping (Result<[User], APIError>) -> Void) {
         // old  method of dealing with concurrency -   compkletion handler
         // @esccaping  because the completion function will escape the timeframe/scope of the calling code
         guard
             let url  =  URL(string: urlString)
         else {
+            completion(.failure(.invalidURL))
             return
         }
         URLSession.shared.dataTask(with: url) {  data, response, error in
@@ -23,26 +24,40 @@ struct APIService {
                 let httpResponse = response as? HTTPURLResponse,
                 httpResponse.statusCode ==  200
             else {
+                completion(.failure(.invalidResponseStatus))
                 return
             }
             guard
                 error ==  nil
             else  {
+                completion(.failure(.dataTaskError))
                 return
             }
             guard
                 let data = data
             else {
+                completion(.failure(.corruptData))
                 return
             }
             
             let decoder  = JSONDecoder()
             do {
                 let decodedData = try decoder.decode([User].self, from: data)
-                completion(decodedData)
+                completion(.success(decodedData))
             } catch {
-                print("Error")
+                completion(.failure(.decodingError))
+                //print("Error")
             }
         }.resume()
     }
+}
+
+// our custom thrown error
+enum APIError:  Error {
+    case invalidURL
+    case invalidResponseStatus
+    case dataTaskError
+    case corruptData
+    case decodingError
+    
 }
