13dd1047fc4d3e16dedd6767e0c5e4af16cb665c
davidakoontz <david@koontz.name>
2022-04-09 13:45:06 -0500

    Initial Commit

---
+++ iOSConcurrency/iOSConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyApp.swift
+//  iOSConcurrency
+//
+//  Created by David on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
